e6f1019c23ff6bab4d308506e52e7c8eb2ef0153
andriwinanda <andriwinanda98@gmail.com>
2026-07-17 16:27:13 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(async ({ mode }) => {
               const username = env.VITE_GOWA_USERNAME
               const password = env.VITE_GOWA_PASSWORD
               proxyReq.setHeader('Authorization', `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`)
+              proxyReq.setHeader('X-Device-Id', `6285111528585@s.whatsapp.net`)
             })
           }
         },
@@ -37,6 +38,7 @@ export default defineConfig(async ({ mode }) => {
               const username = env.VITE_GOWA_USERNAME
               const password = env.VITE_GOWA_PASSWORD
               proxyReq.setHeader('Authorization', `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`)
+              proxyReq.setHeader('X-Device-Id', `6285111528585@s.whatsapp.net`)
             })
           }
         },
